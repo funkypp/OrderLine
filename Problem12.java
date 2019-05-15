413d34301b415ae2bd4f704863a1584099d35146
funkypp <50372286+funkypp@users.noreply.github.com>
2019-05-15 16:06:49 +0200

    Update Problem12.java

--- Problem12.java
+++ Problem12.java
@@ -5,14 +5,15 @@ public class Problem12 {
 
         Order order1 = new Order(1234, "2019-05-02", "34567",
                 "Patrik Petersson\nGrindtorps skolgränd 44\n18347 Stockholm");
+        System.out.println(order1);
         order1.addOrderLine(1234, 1, 1500, 25); // Processor A
         order1.addOrderLine(2345, 3, 500, 25); // Hårddisk B
-        System.out.println(order1);
         order1.addOrderLine(3456, 1, 180, 25); // Mus
-        System.out.println("Ditt totala pris : " + order1.getTotalPrice());
-        
-        
         order1.toPrint();
+        System.out.println("Ditt totala pris : " + order1.getTotalPrice());
+        System.out.println("ArticleID: " + order1.getArticleIDD());
+        order1.getarticleIDZ();
+        
         
     }
 }
